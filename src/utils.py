537fc7d0c919adfa6733222bc3d5c3145acbf993
Fofi <christian.bastiasj@mail.udp.cl>
2023-06-03 14:38:58 -0400

    borra crear tabla de utils

--- src/utils.py
+++ src/utils.py
@@ -14,39 +14,3 @@ def bus_format(data, service_name=''):
         service_name+transformed_data
 
     return str_data_length
-
-def create_tables():
-    conn = sqlite3.connect('db.sqlite3')
-    c = conn.cursor()
-
-    c.execute(
-        '''CREATE TABLE IF NOT EXISTS users
-            (
-                rut text PRIMARY KEY,
-                email text,
-                name text,
-                password text,
-                type integer DEFAULT 0
-            )
-            '''
-
-    )
-    c.execute(
-        '''CREATE TABLE IF NOT EXISTS productos
-            (
-                id integer PRIMARY KEY AUTOINCREMENT,
-                nombre text,
-                precio integer,
-                stock integer,
-                SKU integer,
-                categoria text
-            )
-        '''
-    )
-    
-    conn.commit()
-    conn.close()
-
-
-if __name__ == '__main__':
-    crear_tablas()
