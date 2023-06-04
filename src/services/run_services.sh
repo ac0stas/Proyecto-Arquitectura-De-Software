5724a08442058573b1d7159dabf6bd2c31caffab
Radswet <fvergaralobos@gmail.com>
2023-06-03 20:19:59 -0400

    update run_services.sh

--- src/services/run_services.sh
+++ src/services/run_services.sh
@@ -1,10 +1,4 @@
-trap 'kill %1; kill %2; kill %3; kill %4; kill %5; kill %6; kill %7; kill %8' SIGINT
-python3 services/serv1.py & \
-python3 services/serv2.py & \
-python3 services/serv3.py & \
-python3 services/serv4.py & \
-python3 services/serv5.py & \
-python3 services/serv6.py & \
-python3 services/serv7.py & \
-python3 services/serv8.py & \
-python3 services/serv9.py
+trap 'kill %1; kill %2' SIGINT
+python3 services/buscar_producto.py & \
+python3 services/crear_producto.py & \
+python3 services/eliminar_producto.py
