6500c797224c23f6c0adaedc93a72c947d5ccc71
Joaquinnl22 <joaquin.neira@mail.udp.cl>
2023-06-03 22:19:05 -0400

    Run_Services

--- src/services/run_services.sh
+++ src/services/run_services.sh
@@ -1,4 +1,4 @@
 trap 'kill %1; kill %2; kill %3' SIGINT
-python3 services/serv1.py & \
-python3 services/serv2.py & \
-python3 services/serv3.py 
+python3 serv1.py & \
+python3 serv2.py & \
+python3 serv3.py 
