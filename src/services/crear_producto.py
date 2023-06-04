9d63fd78fb54baf24cf222791a8ef7f1b104b839
Joaquinnl22 <joaquin.neira@mail.udp.cl>
2023-06-03 20:14:51 -0400

    crear_producto

--- src/services/crear_producto.py
+++ src/services/crear_producto.py
@@ -1,4 +1,11 @@
 import sqlite3
+import sys
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = ('localhost', 5000)
+sock.connect(server_address)
+
 
 def create_product(name, price, sku, stock):
     conn = sqlite3.connect('inventario.db')
@@ -6,3 +13,21 @@ def create_product(name, price, sku, stock):
     c.execute("INSERT INTO Product VALUES (?, ?, ?, ?)", (sku, name, price, stock))
     conn.commit()
     conn.close()
+
+try:
+    # Send data
+    message = b"00050sinitservi1"
+    print ('sending {!r}'.format (message))
+    sock.sendall(message)
+    while True:
+        # Look for the response
+        print ("Waiting for transaction")
+        amount_received = 0
+        amount_expected = int(sock.recv (5))
+        while amount_received < amount_expected:
+            data = data.decode().split()
+            print ('received {!r}'.format (data))
+            
+finally:
+    print ('closing socket')
+    sock.close ()
