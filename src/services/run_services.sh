25dcc36ebe32a2294adb0d6938f77ecef09e6d70
Joaquinnl22 <joaquin.neira@mail.udp.cl>
2023-06-03 20:30:57 -0400

    Services_update

--- src/services/run_services.sh
+++ src/services/run_services.sh
@@ -1,4 +1,4 @@
-trap 'kill %1; kill %2' SIGINT
-python3 services/buscar_producto.py & \
-python3 services/crear_producto.py & \
-python3 services/eliminar_producto.py
+trap 'kill %1; kill %2; kill %3' SIGINT
+python3 services/serv1.py & \
+python3 services/serv2.py & \
+python3 services/serv3.py 
